12701523182877bf9359c7d029cdc15e4a7f2757
xx <xx@xx.com>
2017-12-07 21:46:44 +0800

    添加项目文件。

---
+++ ConsoleApplication1/Class1.cs
@@ -0,0 +1,120 @@
+﻿using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Command;
+using SuperSocket.SocketBase.Config;
+using SuperSocket.SocketBase.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ConsoleApplication1
+{
+    public class TelnetSession : AppSession<TelnetSession, YourRequestInfo>
+    {
+        protected override void OnSessionStarted()
+        {
+            this.Send("Welcome to SuperSocket Telnet Server?");
+        }
+
+        protected override void HandleUnknownRequest(YourRequestInfo requestInfo)
+        {
+            this.Send("Unknow request??");
+        }
+
+        protected override void HandleException(Exception e)
+        {
+            this.Send("Application error123: {0}", e.Message);
+        }
+
+        protected override void OnSessionClosed(CloseReason reason)
+        {
+            //add you logics which will be executed after the session is closed
+            base.OnSessionClosed(reason);
+        }
+    }
+
+    public class ECHO : CommandBase<TelnetSession, YourRequestInfo>
+    {
+        public override void ExecuteCommand(TelnetSession session, YourRequestInfo requestInfo)
+        {
+             session.Send(@"sdfsdf
+               AS  ");
+        }
+        public override string Name
+        {
+            get
+            {
+                return base.Name;
+            }
+        }
+    }
+
+    public class PlayerSession : AppSession<PlayerSession>
+    {
+        public int GameHallId { get; internal set; }
+
+        public int RoomId { get; internal set; }
+    }
+
+    public class TelnetServer : AppServer<TelnetSession, YourRequestInfo>
+    {
+        public TelnetServer():base(new YourReceiveFilterFactory()) {
+
+        }
+        protected override bool Setup(IRootConfig rootConfig, IServerConfig config)
+        {
+            return base.Setup(rootConfig, config);
+        }
+
+        protected override void OnStartup()
+        {
+            base.OnStartup();
+        }
+
+        protected override void OnStopped()
+        {
+            base.OnStopped(); 
+        }
+
+
+
+    }
+ public   class YourRequestInfo:IRequestInfo
+    {
+        public string Key { get; set; }
+
+        public int DeviceId { get; set; }
+        string A { set; get; }
+    }
+
+    public class YourReceiveFilterFactory : IReceiveFilterFactory<YourRequestInfo>
+    {
+        //More code
+        public IReceiveFilter<YourRequestInfo> CreateFilter(IAppServer appServer, IAppSession appSession, IPEndPoint remoteEndPoint)
+        {
+            return new YourReceiveFilter(new byte []{ 67,63,76,65},"test");
+        }
+    }
+    public class YourReceiveFilter : TerminatorReceiveFilter<YourRequestInfo>
+    {
+        public YourReceiveFilter(byte[] terminator,string dd) : base(terminator)
+        {
+            Console.WriteLine(dd);
+        }
+
+        //More code
+        public override YourRequestInfo Filter(byte[] readBuffer, int offset, int length, bool toBeCopied, out int rest)
+        {
+            return base.Filter(readBuffer, offset, length, toBeCopied, out rest);
+        }
+
+        protected override YourRequestInfo ProcessMatchedRequest(byte[] data, int offset, int length)
+        {
+            throw new NotImplementedException("NotImplementedException");
+        }
+    }
+
+
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,145 @@
+﻿using SuperSocket.ServerManager;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Command;
+using SuperSocket.SocketBase.Protocol;
+using SuperSocket.SocketEngine;
+using SuperSocket.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApplication1
+{
+    public class ADD : CommandBase<AppSession, StringRequestInfo>
+    {
+        public override void ExecuteCommand(AppSession session, StringRequestInfo requestInfo)
+        {
+            session.Send(requestInfo.Parameters.Select(p => Convert.ToInt32(p)).Sum().ToString());
+        }
+    }
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //var f = new Form();
+            //f.Text = "cccc";
+            //Application.Run(f);
+            ////Console.WriteLine("ff");
+            ////Console.ReadKey();
+            //return;
+            //TelnetServer s = new TelnetServer();
+            //s.Setup(4502);
+            //s.Start();
+            //Console.ReadKey();
+            //return;
+            //Console.ReadKey();
+
+            //s.Stop();
+            //return;
+
+            var f = new SuperSocket.ServerManager.ManagementServer();
+            f.Setup(2012);
+            f.Start();
+            Console.ReadKey();
+            return;
+            Console.WriteLine("Press any key to start the server!");
+
+            Console.ReadKey();
+            Console.WriteLine();
+
+            var bootstrap = BootstrapFactory.CreateBootstrap();
+
+            if (!bootstrap.Initialize())
+            {
+                Console.WriteLine("Failed to initialize!");
+                Console.ReadKey();
+                return;
+            }
+
+            var result = bootstrap.Start();
+
+            Console.WriteLine("Start result: {0}!", result);
+
+            if (result == StartResult.Failed)
+            {
+                Console.WriteLine("Failed to start!");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("Press key 'q' to stop it!");
+
+            while (Console.ReadKey().KeyChar != 'q')
+            {
+                Console.WriteLine();
+                continue;
+            }
+
+            Console.WriteLine();
+
+            //Stop the appServer
+            bootstrap.Stop();
+
+            Console.WriteLine("The server was stopped!");
+            Console.ReadKey();
+        }
+
+
+        static void appServer_NewRequestReceived(AppSession session, StringRequestInfo requestInfo)
+        {
+            switch (requestInfo.Key.ToUpper())
+            {
+                case ("ECHO"):
+                    session.Send(requestInfo.Body);
+                    break;
+
+                case ("ADD"):
+                    session.Send(requestInfo.Parameters.Select(p => Convert.ToInt32(p)).Sum().ToString());
+                    break;
+
+                case ("MULT"):
+
+                    var result = 1;
+
+                    foreach (var factor in requestInfo.Parameters.Select(p => Convert.ToInt32(p)))
+                    {
+                        result *= factor;
+                    }
+
+                    session.Send(result.ToString());
+                    break;
+            }
+        }
+
+        static void appServer_NewRequestReceived(WebSocketSession session, string t)
+        {
+            session.Send(t);
+            return;
+            switch (t.ToUpper())
+            {
+                case ("ECHO"):
+                    session.Send(t);
+                    break;
+
+                case ("ADD"):
+                    session.Send(t);
+                    break;
+                case ("MULT"):
+                    var result = 1;
+                    session.Send(result.ToString());
+                    break;
+            }
+        }
+        static void appServer_NewSessionConnected(AppSession session)
+        {
+            session.Send("Welcome to SuperSocket Telnet Server");
+        }
+
+    }
+}
