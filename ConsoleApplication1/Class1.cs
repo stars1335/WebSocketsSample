8c37dad7fc2d5f25c88e07ed60ffaaafa8b4f795
xx <xx@xx.com>
2017-12-08 22:23:25 +0800

    额鹅鹅鹅

--- ConsoleApplication1/Class1.cs
+++ ConsoleApplication1/Class1.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Diagnostics.Contracts;
 
-namespace ConsoleApplication1
+namespace ConsoleApplication1  
 {
     public class TelnetSession : AppSession<TelnetSession, YourRequestInfo>
     {
